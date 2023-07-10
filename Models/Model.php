d8e8b126e06d88a9629350468da362d1358f22f8
Angel Torres <angtor.dev@gmail.com>
2023-07-09 20:19:43 -0400

    Se implementa metodo para cargar una unica instancia de la bd

--- Models/Model.php
+++ Models/Model.php
@@ -20,6 +20,21 @@ class Model
         
         return $stmt->fetchAll();
     }
+
+    public static function cargar(int $id) : null|object
+    {
+        $bd = new Database();
+        $table = static::class;
+        $query = "SELECT * FROM $table WHERE id = $id";
+
+        $stmt = $bd->connect()->query($query);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, $table);
+
+        if ($stmt->rowCount() == 0) {
+            return null;
+        }
+        return $stmt->fetch();
+    }
     }
 
     protected function query(String $query) : PDOStatement {
