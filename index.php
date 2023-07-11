06058417dc9a3fdfb2614052bbc492c50ae13014
Angel Torres <angtor.dev@gmail.com>
2023-07-10 21:04:42 -0400

    Se refactoriza frontController para que redirija a controlador por defecto en caso de no especificarse. Se corrige error que se producia cuando la ruta terminaba en "/"

--- index.php
+++ index.php
@@ -2,12 +2,21 @@
 require_once "config.php";
 require_once "utilities.php";
 
+$defaultController = "Index";
+$defaultPath = "Home/";
+
+// Carpeta relativa actual del proyecto
 $relativePath = substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], "index.php"));
+// Elimina la ruta del proyecto, los parametro y el ultimo "/"
 $requestUri = substr($_SERVER['REQUEST_URI'], strlen($relativePath));
 $requestUri = substr($requestUri, 0, strpos($requestUri, '?') === false ? strlen($requestUri) : strpos($requestUri, '?'));
+$requestUri = rtrim($requestUri, "/");
+// Divide la ruta en partes
 $uriParts = explode('/', $requestUri);
-$controllerName = count($uriParts) > 1 ? $uriParts[count($uriParts) - 1] : "Index";
-$controllerPath = substr($requestUri, 0, strpos($requestUri, $controllerName)) ?: $requestUri."/";
+
+$controllerName = count($uriParts) > 1 ? $uriParts[count($uriParts) - 1] : $defaultController;
+$controllerPath = empty($requestUri) ? $defaultPath :
+    (substr($requestUri, 0, strpos($requestUri, $controllerName)) ?: $requestUri."/");
 
 if (is_file("Controllers/".$controllerPath.$controllerName."Controller.php")) {
     require_once "Controllers/".$controllerPath.$controllerName."Controller.php";
