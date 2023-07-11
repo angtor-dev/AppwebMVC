96045394c4c8743b645e8241486eda6749c29d71
Angel Torres <angtor.dev@gmail.com>
2023-07-10 21:25:17 -0400

    Se crea el modelo Usuario

---
+++ Models/Usuario.php
@@ -0,0 +1,57 @@
+<?php
+require_once "Models/Model.php";
+
+class Usuario extends Model
+{
+    public int $id;
+    public string $correo;
+    public string $clave;
+    public string $nombre;
+    public string $apellido;
+    public string $registro;
+    public ?string $ultimoAcceso;
+    public string $ultimaActualizacion;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function login(string $correo, string $clave) : bool
+    {
+        if (empty($correo) || empty($clave)) {
+            return false;
+        }
+
+        $query = "SELECT * FROM usuario WHERE correo = :correo";
+        
+        try {
+            $stmt = $this->prepare($query);
+            $stmt->bindValue('correo', $correo);
+
+            $stmt->execute();
+
+            if ($stmt->rowCount() == 0) {
+                return false;
+            }
+
+            $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if (!password_verify($clave, $usuario['clave'])) {
+                return false;
+            }
+
+            foreach ($usuario as $key => $value) {
+                if (property_exists($this, $key)) {
+                    $this->$key = $value;
+                }
+            }
+            return true;
+        } catch (\Throwable $th) {
+            http_response_code(500);
+            throw $th;
+        }
+        return true;
+    }
+}
+?>
