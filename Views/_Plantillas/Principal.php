10d84ee3410f8024d18e0990416b2296c0d0bbba
Angel Torres <angtor.dev@gmail.com>
2023-07-11 23:05:07 -0400

    Se incluye libreria de iconos FontAwesone 6.4.0

--- Views/_Plantillas/Principal.php
+++ Views/_Plantillas/Principal.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$title ?? ""?> - <?=APP_NAME?></title>
+    <title><?= $title ?? "" ?> - <?= APP_NAME ?></title>
+    <link rel="stylesheet" href="/AppwebMVC/public/lib/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="/AppwebMVC/public/lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/AppwebMVC/public/css/site.css">
 </head>
