8daf0000ad080d899bfc8dc951149f7cbf19003a
Angel Torres <angtor.dev@gmail.com>
2023-08-21 22:42:40 -0400

    Se agrega enum para Paises

---
+++ Enums/Pais.php
@@ -0,0 +1,17 @@
+<?php
+enum Pais: string
+{
+    case Argentina = 'ar';
+    case Bolivia   = 'bo';
+    case Brasil    = 'br';
+    case Chile     = 'cl';
+    case Colombia  = 'co';
+    case Ecuador   = 'ec';
+    case Guyana    = 'gy';
+    case Paraguay  = 'py';
+    case Peru      = 'pr';
+    case Suriname  = 'sr';
+    case Uruguay   = 'uy';
+    case Venezuela = 've';
+}
+?>
