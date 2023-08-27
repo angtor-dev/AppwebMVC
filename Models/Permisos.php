b77610cbd9a102dca3be4b240e6d27b797a600a4
Angel Torres <angtor.dev@gmail.com>
2023-08-27 18:23:29 -0400

    Correción de tipeo

--- Models/Permisos.php
+++ Models/Permisos.php
@@ -4,7 +4,7 @@ require_once "Models/Model.php";
 class Permisos extends Model
 {
     public int $id;
-    public bool $consulaUsuarios;
+    public bool $consultaUsuarios;
     public bool $registraUsuarios;
     public bool $actualizaUsuarios;
     public bool $eliminaUsuarios;
