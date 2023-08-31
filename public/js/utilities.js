fe9da5b2399da9d234192d34322a14150e27e262
Angel Torres <angtor.dev@gmail.com>
2023-08-31 10:51:59 -0400

    Se implementa función para generar alertas de Bootstrap

--- public/js/utilities.js
+++ public/js/utilities.js
@@ -9,3 +9,21 @@ document.querySelectorAll('.acordeon-toggle').forEach(a => {
     a.addEventListener('click', e =>
         e.currentTarget.parentElement.classList.toggle('show'))
 })
+
+/* Funciones de utilidades */
+
+/**
+ * Genera una alerta de Bootstrap
+ * @param {string} mensaje Mensaje a mostrar
+ * @param {string} tipo Tipo de alerta basado en las clases de bootstrap
+ * (Ej. primary, secondary, success...). "danger" por defecto.
+ */
+function alerta(mensaje, tipo = "danger") {
+    let wrapper = document.getElementById('alerts-section')
+
+    wrapper.innerHTML += `
+    <div class="alert alert-${tipo} alert-dismissible fade show" role="alert">
+        ${mensaje}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>`;
+}
