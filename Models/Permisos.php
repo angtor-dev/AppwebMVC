7c6262e1dcdabc134c2d27297de6407c4d28a753
Angel Torres <angtor.dev@gmail.com>
2023-09-05 08:30:50 -0400

    Se agregan nuevos permisos

--- Models/Permisos.php
+++ Models/Permisos.php
@@ -4,28 +4,46 @@ require_once "Models/Model.php";
 class Permisos extends Model
 {
     public int $id;
-    public bool $consultaUsuarios = false;
-    public bool $registraUsuarios = false;
-    public bool $actualizaUsuarios = false;
-    public bool $eliminaUsuarios = false;
+    public bool $consultarUsuarios = false;
+    public bool $registrarUsuarios = false;
+    public bool $actualizarUsuarios = false;
+    public bool $eliminarUsuarios = false;
+    public bool $consultarRoles = false;
+    public bool $registrarRoles = false;
+    public bool $actualizarRoles = false;
+    public bool $eliminarRoles = false;
+    public bool $gestionarPermisos = false;
+    public bool $consultarBitacora = false;
 
     public Rol $rol;
 
     public function actualizar() : void
     {
         $query = "UPDATE permisos SET
-            consultaUsuarios = :consultaUsuarios,
-            registraUsuarios = :registraUsuarios,
-            actualizaUsuarios = :actualizaUsuarios,
-            eliminaUsuarios = :eliminaUsuarios
+            consultarUsuarios = :consultarUsuarios,
+            registrarUsuarios = :registrarUsuarios,
+            actualizarUsuarios = :actualizarUsuarios,
+            eliminarUsuarios = :eliminarUsuarios,
+            consultarRoles = :consultarRoles,
+            registrarRoles = :registrarRoles,
+            actualizarRoles = :actualizarRoles,
+            eliminarRoles = :eliminarRoles,
+            gestionarPermisos = :gestionarPermisos,
+            consultarBitacora = :consultarBitacora,
             WHERE id = $this->id";
 
         try {
             $stmt = $this->prepare($query);
-            $stmt->bindValue('consultaUsuarios', $this->consultaUsuarios);
-            $stmt->bindValue('registraUsuarios', $this->registraUsuarios);
-            $stmt->bindValue('actualizaUsuarios', $this->actualizaUsuarios);
-            $stmt->bindValue('eliminaUsuarios', $this->eliminaUsuarios);
+            $stmt->bindValue('consultarUsuarios', $this->consultarUsuarios);
+            $stmt->bindValue('registrarUsuarios', $this->registrarUsuarios);
+            $stmt->bindValue('actualizarUsuarios', $this->actualizarUsuarios);
+            $stmt->bindValue('eliminarUsuarios', $this->eliminarUsuarios);
+            $stmt->bindValue('consultarRoles', $this->consultarRoles);
+            $stmt->bindValue('registrarRoles', $this->registrarRoles);
+            $stmt->bindValue('actualizarRoles', $this->actualizarRoles);
+            $stmt->bindValue('eliminarRoles', $this->eliminarRoles);
+            $stmt->bindValue('gestionarPermisos', $this->gestionarPermisos);
+            $stmt->bindValue('consultarBitacora', $this->consultarBitacora);
 
             $stmt->execute();
         } catch (\Throwable $th) {
