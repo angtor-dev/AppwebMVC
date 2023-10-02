6f625451d656362d72fc6b6a6168de572443ac1e
Angel Torres <angtor.dev@gmail.com>
2023-10-02 15:23:57 -0400

    Se dividen grupos por estado

--- Views/Grupos/Index.php
+++ Views/Grupos/Index.php
@@ -19,30 +19,66 @@ $title = "Grupos";
     <h5 class="border-bottom pb-2 mb-3">Grupos Activos</h5>
     <div class="row gy-3">
         <?php foreach ($grupos as $grupo): ?>
-            <div class="col-12 col-lg-6 col-xl-4">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="card-title fw-medium mb-0"><?= $grupo->getNombre() ?></div>
-                    </div>
-                    <div class="card-body">
-                        <b>Nivel:</b> <?= $grupo->subnivel->nivelCrecimiento->getNombre() ?> <br>
-                        <b>Profesor:</b> <?= $grupo->profesor->getNombreCompleto() ?> <br>
-                        <b>Participantes:</b> 0
-                    </div>
-                    <div class="card-footer text-end">
-                        <button class="btn btn-danger px-2" title="Eliminar" data-bs-toggle="modal" data-bs-target="#confirmar-eliminacion"
-                                data-id="<?= $grupo->id ?>">
-                            <i class="fa-solid fa-trash fa-fw"></i>
-                        </button>
-                        <a href="/AppwebMVC/Grupos/Registrar?id=<?= $grupo->id ?>" class="btn btn-primary px-2">
-                            <i class="fa-solid fa-pen fa-fw"></i>
-                        </a>
-                        <a href="/AppwebMVC/Grupos/Gestionar?id=<?= $grupo->id ?>" class="btn btn-secondary px-2">
-                            Gestionar
-                        </a>
+            <?php if ($grupo->getEstado() == EstadosGrupo::Activo->value) : ?>
+                <div class="col-12 col-lg-6 col-xl-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-title fw-medium mb-0"><?= $grupo->getNombre() ?></div>
+                        </div>
+                        <div class="card-body">
+                            <b>Nivel:</b> <?= $grupo->subnivel->nivelCrecimiento->getNombre() ?> <br>
+                            <b>Profesor:</b> <?= $grupo->profesor->getNombreCompleto() ?> <br>
+                            <b>Participantes:</b> 0
+                        </div>
+                        <div class="card-footer text-end">
+                            <button class="btn btn-danger px-2" title="Eliminar" data-bs-toggle="modal" data-bs-target="#confirmar-eliminacion"
+                                    data-id="<?= $grupo->id ?>">
+                                <i class="fa-solid fa-trash fa-fw"></i>
+                            </button>
+                            <a href="/AppwebMVC/Grupos/Registrar?id=<?= $grupo->id ?>" class="btn btn-primary px-2">
+                                <i class="fa-solid fa-pen fa-fw"></i>
+                            </a>
+                            <a href="/AppwebMVC/Grupos/Gestionar?id=<?= $grupo->id ?>" class="btn btn-secondary px-2">
+                                Gestionar
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            <?php endif ?>
+        <?php endforeach ?>
+    </div>
+</div>
+
+<div class="card border-0 shadow p-3">
+    <h5 class="border-bottom pb-2 mb-3">Grupos finalizados</h5>
+    <div class="row gy-3">
+        <?php foreach ($grupos as $grupo): ?>
+            <?php if ($grupo->getEstado() == EstadosGrupo::Finalizado->value) : ?>
+                <div class="col-12 col-lg-6 col-xl-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-title fw-medium mb-0"><?= $grupo->getNombre() ?></div>
+                        </div>
+                        <div class="card-body">
+                            <b>Nivel:</b> <?= $grupo->subnivel->nivelCrecimiento->getNombre() ?> <br>
+                            <b>Profesor:</b> <?= $grupo->profesor->getNombreCompleto() ?> <br>
+                            <b>Participantes:</b> 0
+                        </div>
+                        <div class="card-footer text-end">
+                            <button class="btn btn-danger px-2" title="Eliminar" data-bs-toggle="modal" data-bs-target="#confirmar-eliminacion"
+                                    data-id="<?= $grupo->id ?>">
+                                <i class="fa-solid fa-trash fa-fw"></i>
+                            </button>
+                            <a href="/AppwebMVC/Grupos/Registrar?id=<?= $grupo->id ?>" class="btn btn-primary px-2">
+                                <i class="fa-solid fa-pen fa-fw"></i>
+                            </a>
+                            <a href="/AppwebMVC/Grupos/Gestionar?id=<?= $grupo->id ?>" class="btn btn-secondary px-2">
+                                Detalles
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            <?php endif ?>
         <?php endforeach ?>
     </div>
 </div>
@@ -65,11 +101,5 @@ $title = "Grupos";
                 <a href="#" data-href="/AppwebMVC/Grupos/Eliminar?id=" type="button" class="btn btn-danger btn-eliminar">Si, eliminar</a>
             </div>
         </div>
-    </div>
-</div>
-
-<div class="card border-0 shadow p-3">
-    <h5 class="border-bottom pb-2 mb-3">Grupos finalizados</h5>
-    <div class="row gy-3">
     </div>
 </div>
