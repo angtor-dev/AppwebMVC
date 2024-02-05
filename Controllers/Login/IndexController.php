be5a89d7401f7b6628b792d716a3e194f8571d72
JesusCanelon <canelonaguilar.dev@gmail.com>
2024-02-05 11:41:56 -0400

    cambio controlador login

--- Controllers/Login/IndexController.php
+++ Controllers/Login/IndexController.php
@@ -5,14 +5,19 @@ $loginFails = false;
 if (!empty($_POST)) {
     $usuario = new Usuario();
 
-    if ($usuario->login($_POST['cedula'], $_POST['clave'])) {
-        Bitacora::registrar("Inicio de sesión");
-        http_response_code(200);
-        header('location:'.LOCAL_DIR);
-        exit();
-    }
-    else {
-        http_response_code(402);
+    if (isset($_POST['cedula']) && isset($_POST['clave'])) {
+        if ($usuario->login($_POST['cedula'], $_POST['clave'])) {
+            Bitacora::registrar("Inicio de sesión");
+            http_response_code(200);
+            header('location:' . LOCAL_DIR);
+            exit();
+        } else {
+            http_response_code(402);
+            $loginFails = true;
+        }
+
+    } else {
+        http_response_code(403);
         $loginFails = true;
     }
 }
