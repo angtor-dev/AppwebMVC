556df38a6948d8a061b80a77640b1be7f9a5f9df
JesusCanelon <canelonaguilar.dev@gmail.com>
2024-03-01 21:34:32 -0400

    Empezando el recuperar password

--- Views/Login/Index.php
+++ Views/Login/Index.php
@@ -10,36 +10,68 @@
             </div>
 
             <div id="cartaLogin">
-                    <div class="d-flex align-items-center justify-content-center p-3">
-                        <div class="content-avatar">
-                            <img src="./public/img/logo.png" alt="logo" class="avatar">
+                <div class="d-flex align-items-center justify-content-center p-3">
+                    <div class="content-avatar">
+                        <img src="./public/img/logo.png" alt="logo" class="avatar">
+                    </div>
+                </div>
+
+                <form action="<?= LOCAL_DIR ?>Login" class="mt-4" method="post" id="loginForm">
+                    <div class="d-grid gap-4 mb-5">
+                        <div class="d-flex flex-column align-items-center">
+                            <label class="form-label fw-bold text-white">Cedula</label>
+                            <input type="text" class="inputLogin text-center" name="cedula" maxlength="8" required>
+                        </div>
+
+                        <div class="d-flex flex-column align-items-center">
+                            <label class="form-label text-white fw-bold">Clave</label>
+                            <input type="password" class="inputLogin text-center" name="clave" maxlength="10" required>
                         </div>
                     </div>
+                    <div class="d-flex justify-content-center">
+                        <button type="submit" id="botonLogin" class="btn w-auto text-white">
+                            INICIAR SESION
+                        </button>
+                    </div>
+                </form>
 
-                    <form action="<?= LOCAL_DIR ?>Login" class="mt-4" method="post" id="loginForm">
-                        <div class="d-grid gap-4 mb-5">
-                            <div class="d-flex flex-column align-items-center">
-                                <label class="form-label fw-bold text-white">Cedula</label>
-                                <input type="text" class="inputLogin text-center" name="cedula" maxlength="8" required>
-                            </div>
+                <?php if ($loginFails): ?>
+                    <div class="alert alert-light" role="alert">
+                        <p class="text-danger text-center">La cédula o la clave son incorrectos</p>
+                    </div>
+                <?php endif ?>
 
-                            <div class="d-flex flex-column align-items-center">
-                                <label class="form-label text-white fw-bold">Clave</label>
-                                <input type="password" class="inputLogin text-center" name="clave" maxlength="10" required>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-center">
-                            <button type="submit" id="botonLogin" class="btn w-auto text-white">
-                                INICIAR SESION
-                            </button>
-                        </div>
-                    </form>
+                <div class="d-grid mt-3">
+                    <a class="text-center text-white" href="#" data-bs-toggle="modal"
+                        data-bs-target="#exampleModal">Recuperar contraseña</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-                    <?php if ($loginFails) : ?>
-                        <div class="alert alert-light" role="alert">
-                            <p class="text-danger text-center">La cédula o la clave son incorrectos</p>
+
+<!-- Modal para Recuperar password -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Recuperar contraseña</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= LOCAL_DIR ?>Login" class="mt-4" method="post" id="loginForm">
+                    <div class="d-grid gap-4 mb-5">
+                        <div class="d-flex flex-column">
+                            <label class="form-label fw-bold text-center">Ingrese su correo</label>
+                            <input class="form-control" type="email" name="correoRecuperacion">
                         </div>
-                    <?php endif ?>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Enviar</button>
             </div>
         </div>
     </div>
