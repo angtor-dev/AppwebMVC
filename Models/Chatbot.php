d9b011ebe29e7575b8674cde8ed1317a5818ce59
AnjhelE <136629574+AnjhelE@users.noreply.github.com>
2024-11-28 22:18:38 -0400

    reglas

--- Models/Chatbot.php
+++ Models/Chatbot.php
@@ -8,18 +8,58 @@ require_once "Models/Model.php";
  */
 class Chatbot extends Model
 {
-    private $sinonimos = [
-        'realizar' => 'hacer',
-    'efectuar' => 'hacer',
-    'ejecutar' => 'hacer',
+    //Diccionarios de sinónimos y palabras clave
+    private $palabrasClave = [
+        'enumerar' => 'listar',
+    'catalogar' => 'listar',
+    'consultar' => 'listar',
+    'ver' => 'listar',
+    'renovar' => 'actualizar',
+    'editar' => 'actualizar',
+    'cambiar' => 'actualizar',
+    'modificar' => 'actualizar',
+    'revisar' => 'actualizar',
     'inscribir' => 'registrar',
     'anotar' => 'registrar',
     'documentar' => 'registrar',
     'borrar' => 'eliminar',
     'suprimir' => 'eliminar',
     'quitar' => 'eliminar',
-    'anjhel' => 'eliminar'
+    'calendario' => 'agenda'
+
     ];
+
+    private $sinonimos = [
+    'clave' => 'contrasena',
+    'buscar' => 'encontrar',
+    'ver' => 'observar',
+    'usar' => 'emplear',
+    'ayudame' => 'ayuda',
+    'crear' => 'generar',
+    'eliminar' => 'borrar',
+    'actualizar' => 'renovar',
+    'iniciar' => 'comenzar',
+    'terminar' => 'finalizar',
+    'guardar' => 'almacenar',
+    'cuenta' => 'usuario',
+    'perfil' => 'información',
+    'datos' => 'información',
+    'problema' => 'inconveniente',
+    'ayuda' => 'asistencia',
+    'configuración' => 'ajustes',
+    'opción' => 'alternativa',
+    'saludos' => 'hola',
+    'jelou' => 'hola',
+    'adiós' => 'hasta luego',
+    'gracias' => 'agradezco',
+    'por favor' => 'favor',
+    ];
+
+    
+    private $modulos = ['sede', 'agenda', 'territorio', 'sedes', 'agendas', 'territorios', 'clave', 'contrasena'];
+
+    private $acciones = ['listar', 'eliminar', 'actualizar', 'registrar', 'recuperar'];
+    
     /**
      * Obtiene una respuesta de la base de datos para una pregunta dada.
      * Utiliza una búsqueda de texto completo en la columna 'question' de la tabla 'chatbot_conocimiento'.
@@ -32,23 +72,41 @@ class Chatbot extends Model
     {
         // Preprocesar la pregunta para normalizarla antes de buscarla en la base de datos
         $question = $this->preprocesarPregunta($pregunta);
-        
-
-        // Realizar la búsqueda inicial
-    $respuesta = $this->buscarRespuestaEnBaseDeDatos($question);
-
-    // Si se encontró una respuesta, devolverla
-    if ($respuesta) {
-        return $respuesta;
-    } else {
-
-    // Si no se encontró, intentar con sinónimos
-    $respuesta = $this->nuevapregunta($question);
          
-        return $respuesta ?: 'Lo siento, no tengo una respuesta para eso.';
+        //Reglas implícitas
+        $accion = $this->buscarAcciones($question);
+        $modulo = $this->buscarModulos($question);
 
-     }
-    // Si aún no se encontró respuesta, devolver un mensaje por defecto
+
+        if (!empty($modulo) && empty($accion)) {
+            // Módulo está lleno y acción está vacía
+            return "Claro que te puedo proporcionar toda la información que necesites sobre $modulo. Solo necesito que ahora especifiques qué deseas hacer, ejemplo: eliminar, editar, actualizar...";
+        } elseif (empty($modulo) && !empty($accion)) {
+            // Módulo está vacío y acción está llena
+            return "Necesitas ser más específico sobre qué deseas $accion para poder ayudarte.";
+        } else {
+            // Ninguna de las condiciones anteriores se cumple
+
+            // Realizar la búsqueda inicial
+        $respuesta = $this->buscarRespuestaEnBaseDeDatos($question, $accion, $modulo);
+
+        if ($respuesta){
+         return $respuesta;
+ 
+        } else {
+ 
+        // Realizar seguhnda busqueda con Sinonimos
+         $question = $this->buscarConSinonimos($question);
+         $accion = $this->buscarAcciones($question);
+         $modulo = $this->buscarModulos($question);
+         $respuesta = $this->buscarRespuestaEnBaseDeDatos($question, $accion, $modulo);
+ 
+         // Si aún no se encontró respuesta, devolver un mensaje por defecto
+         return $respuesta ?: 'Lo siento, no tengo una respuesta para eso.';
+        }
+       
+        }
+     
     
     }
 
@@ -76,65 +134,128 @@ class Chatbot extends Model
         // 4. Eliminar espacios extras al inicio y final, y reducir múltiples espacios internos a uno solo
         $pregunta = trim(preg_replace('/\s+/', ' ', $pregunta));
 
+        $pregunta = $this->obtenerPreguntaConPalabrasClave($pregunta);
+
         return $pregunta;
     }
 
 
-    function nuevapregunta($pregunta) {
-     // Accedemos a la base de conocimientos global
-    
-
-     $nuevaPregunta = $this->obtenerPreguntaConSinonimos($pregunta);
-     $respuesta = $this->buscarRespuestaEnBaseDeDatos($nuevaPregunta);
-
-     return $respuesta;
-       
-    }
-
-    private function obtenerPreguntaConSinonimos($pregunta) {
-        // 1. Desglosar la pregunta en un array de palabras
+    private function obtenerPreguntaConPalabrasClave($pregunta) {
+ 
     $arrayPregunta = explode(' ', $pregunta);
 
-    // 2. Inicializar la nueva pregunta
+   
     $nuevaPregunta = '';
 
-    // 3. Iterar sobre cada palabra de la pregunta y buscar su sinónimo
+    
     foreach ($arrayPregunta as $palabra) {
-        // Convertir la palabra a minúsculas para una comparación más robusta
+        
         $palabraMinuscula = strtolower($palabra);
-
-        // Buscar el sinónimo en el diccionario
-        $sinonimo = $this->sinonimos[$palabraMinuscula] ?? $palabraMinuscula;
-
-        // Agregar el sinónimo o la palabra original a la nueva pregunta
-        $nuevaPregunta .= $sinonimo . ' ';
+        $palabrasClave = $this->palabrasClave[$palabraMinuscula] ?? $palabraMinuscula;
+        $nuevaPregunta .= $palabrasClave . ' ';
     }
 
-    // Eliminar el último espacio sobrante
     $nuevaPregunta = trim($nuevaPregunta);
 
     return $nuevaPregunta;
        }
 
 
-    private function buscarRespuestaEnBaseDeDatos($question)
+    private function buscarRespuestaEnBaseDeDatos($question, $accion, $modulo)
 {
-    // Consulta SQL utilizando MATCH...AGAINST para búsqueda de texto completo en 'question'. Esto es mas eficiente que solo usar la consulta LIKE
+        // Consulta SQL utilizando MATCH...AGAINST para búsqueda de texto completo en 'question'. Esto es mas eficiente que solo usar la consulta LIKE
         // Esto requiere que la columna 'question' tenga un índice FULLTEXT previamente creado.
 
-        $question = "%$question%";  // Formato para búsqueda con LIKE
+        $question = "%$question%";  
+        $modulo = $modulo ? "%$modulo%" : '';
+        $accion = $accion ? "%$accion%" : '';// Formato para búsqueda con LIKE
 
         $sql = "SELECT answer FROM chatbot_conocimiento WHERE MATCH(question) AGAINST (:question IN NATURAL LANGUAGE MODE)";
+
+        $sql .= (!empty($modulo))  ? " AND MATCH(modulo) AGAINST (:modulo IN NATURAL LANGUAGE MODE)" : '';
+        $sql .= (!empty($accion)) ? " AND MATCH(accion) AGAINST (:accion IN NATURAL LANGUAGE MODE)" : '';
+
         $statement = $this->db->pdo()->prepare($sql);
         $statement->bindParam(':question', $question, PDO::PARAM_STR);
+        !empty($modulo) ? $statement->bindParam(':modulo', $modulo,PDO::PARAM_STR) : '';
+        !empty($accion) ? $statement->bindParam(':accion', $accion,PDO::PARAM_STR) : '';
         $statement->execute();
 
         // Obtener el resultado y devolver la respuesta
         $respuesta = $statement->fetch(PDO::FETCH_ASSOC);
 
-        return $respuesta ? $respuesta['answer'] : false;
+        return $respuesta ? $respuesta['answer'] : '';
 }
 
+private function buscarAcciones($question)
+{
+    $arrayPregunta = explode(' ', $question);
+
+  
+    $accionesQuestion = '';
+
+   
+    foreach ($arrayPregunta as $palabra) {
+        $palabraMinuscula = strtolower($palabra);
+
+        if (in_array($palabraMinuscula, $this->acciones)) {
+         
+            $accionesQuestion .= $palabraMinuscula . ' ';
+        }
+    }
+
+    $accionesQuestion = trim($accionesQuestion);
+
+    return $accionesQuestion;
+}
+
+private function buscarModulos($question)
+{
+
+    $arrayPregunta = explode(' ', $question);
+
+    $modulosQuestion = '';
+
+  
+    foreach ($arrayPregunta as $palabra) {
+        $palabraMinuscula = strtolower($palabra);
+
+        if (in_array($palabraMinuscula, $this->modulos)) {
+       
+            $modulosQuestion .= $palabraMinuscula . ' ';
+        }
+    }
+
+    $modulosQuestion = trim($modulosQuestion);
+    return $modulosQuestion;
+    
+}
+
+
+private function buscarConSinonimos($question){
+
+
+    $arrayPregunta = explode(' ', $question);
+
+ 
+    $nuevaPregunta = '';
+
+
+    foreach ($arrayPregunta as $palabra) {
+      
+        $palabraMinuscula = strtolower($palabra);
+
+        $sinonimo = $this->sinonimos[$palabraMinuscula] ?? $palabraMinuscula;
+
+        $nuevaPregunta .= $sinonimo . ' ';
+    }
+
+    $nuevaPregunta = trim($nuevaPregunta);
+
+    return $nuevaPregunta;
+
+
+}
 
 
 
